fcd2684ba6edc9d96e47896e8103001c087142bc
nikunjchhatani <nikunjchhatani@gmail.com>
2024-05-12 13:10:16 +0530

    Task 1

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,10 +1,10 @@
-
-const bookService = require('../services/bookService');
+// controllers/bookController.js
+const axios = require('axios');
 
 exports.getBookList = async (req, res) => {
   try {
-    const books = await bookService.getBookList();
-    res.json(books);
+    const response = await axios.get('http://localhost:3000/api/books');
+    res.json(response.data);
   } catch (error) {
     res.status(500).send(error.message);
   }
