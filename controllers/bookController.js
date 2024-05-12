e86990cdb06ad8f0dd56523c1449ea65239e6458
nikunjchhatani <nikunjchhatani@gmail.com>
2024-05-12 13:11:56 +0530

    Task 2

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,9 +1,19 @@
-// controllers/bookController.js
 const axios = require('axios');
 
 exports.getBookList = async (req, res) => {
   try {
     const response = await axios.get('http://localhost:3000/api/books');
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+
+exports.getBookByISBN = async (req, res) => {
+  const isbn = req.params.isbn;
+  try {
+    const response = await axios.get(`http://localhost:3000/api/books/isbn/${isbn}`);
     res.json(response.data);
   } catch (error) {
     res.status(500).send(error.message);
